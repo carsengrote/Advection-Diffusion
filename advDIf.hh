eaaf8cb2431ae4c63c559b701bbce47b36329d14
MarkHan147 <66350720+MarkHan147@users.noreply.github.com>
2023-12-02 15:06:01 -0600

    Update advDIf.hh

--- advDIf.hh
+++ advDIf.hh
@@ -6,18 +6,21 @@
 // i,j,k are x,y,z direction index
 // L,W,H are x,y,z direction array length
 
-// Points at index 0 or index array length - 1 are ghost nodes
+void start(double LL, double WW, double HH, double dx);
+std::array<double, 3> convert(int i, int j, int k, double dx);
 
 double initializeC(int i, int j, int k); // return VALUE
-double initializeUx(int i, int j, int k, double dx, int L, int W, int H);
-double initializeUy(int i, int j, int k, double dx, int L, int W, int H);
-double initializeUz(int i, int j, int k, double dx, int L, int W, int H);
+double initializeUx(int i, int j, int k);
+double initializeUy(int i, int j, int k);
+double initializeUz(int i, int j, int k);
 
-bool isBoundary(int i, int j, int k, int L, int W, int H); // given index and bound
+bool isBoundary(int i, int j, int k); // given index and bound
 
 double getC(int i, int j, int k);
 std::array<double, 3> getU(int i, int j, int k); // return ux, uy, uz at one point
 
 double updateC(int i, int j, int k, double dx, double dt);
+
+double CFL(double dx);
 
 #endif
