a81367d0c84538f8a2f23f2936ee83f6b10e8477
MarkHan147 <66350720+MarkHan147@users.noreply.github.com>
2023-12-02 13:46:51 -0600

    Create advDIf.hh

---
+++ advDIf.hh
@@ -0,0 +1,21 @@
+#ifndef ADVDIF_HH
+#define ADVDIF_HH
+
+#include <array>
+
+// i,j,k are x,y,z direction index
+// L,W,H are x,y,z direction array length
+
+double initializeC(int i, int j, int k); // return VALUE
+double initializeUx(int i, int j, int k);
+double initializeUy(int i, int j, int k);
+double initializeUz(int i, int j, int k);
+
+bool isBoundary(int i, int j, int k, int L, int W, int H); // given index and bound
+
+double getC(int i, int j, int k);
+std::array<double, 3> getU(int i, int j, int k); // return ux, uy, uz at one point
+
+double updateC(int i, int j, int k, double dx, double dt);
+
+#endif
