6060e59173ff7d2307b7cb3c7e91a23e9cf8dbd7
Carsen Grote <43054768+carsengrote@users.noreply.github.com>
2023-12-11 14:58:14 -0600

    Update videoScript.py

--- videoScript.py
+++ videoScript.py
@@ -1,9 +1,29 @@
 import os
-import sys
+import sys 
+import numpy as np
+
+def worker(startIndex,endIndex,dt, tId):
+    
+    time = startIndex * dt
+    for i in range(startIndex, endIndex + 1):
+        outStr = str(i)
+        if i < 10: 
+            outStr = "000" + outStr
+        elif i < 100:
+            outStr = "00" + outStr
+        elif i < 1000:
+            outStr = "0" + outStr
+
+        # Making plots
+        cmd = "gnuplot -e \"set terminal jpeg; set view map; set cbrange [0:0.005]; set title 't="+str(round(time,1)) + "'; splot 'out' matrix index " + str(int(i)) + " with pm3d \"> ./images/fr"+ outStr + "\.jpeg"
+        
+        os.system(cmd)
+        time = time + dt
+
+    print("Thread " + str(tId) + " finished")
 
 numImages = int(sys.argv[1]) # First CLA is total number of images
 dt = float(sys.argv[2]) # Second CLA is time step
-time = 0
 
 if (not os.path.isdir("images")):
     os.system("mkdir images")
@@ -12,22 +32,29 @@ else:
 
 os.system("rm -f out.mp4") # Remove old video if it exists
 
-print("--Generating Images--")
-for i in range(1,numImages+1):
+numThreads = 10
+imagesPerThread = int(np.floor(numImages/numThreads))
+leftOver = np.mod(numImages,numThreads)
 
-    # Need to pad image name with zeros for correct ordering
-    outStr = str(i)
-    if i < 10:
-        outStr = "00" + outStr
-    elif i < 100:
-        outStr = "0" + outStr
-
-    # Making plots
-    cmd = "gnuplot -e \"set terminal jpeg; set view map; set title 't="+str(round(time,1)) + "'; splot 'out' matrix index " + str(int(i)-1) + " with pm3d \"> ./images/fr"+ outStr + "\.jpeg"
-    os.system(cmd)
-    time = time + dt
+curIndex = 0
+for t in range(0,numThreads): 
     
+    j = os.fork()
+    if j == 0: 
+        if t == numThreads-1:
+            worker(curIndex, numImages, dt, t)
+        else:
+            worker(curIndex, curIndex+imagesPerThread - 1, dt,t)
+        os._exit(0)
+    
+    curIndex = curIndex + imagesPerThread
+
+print("All " +  str(numThreads) + " threads launched")
+
+for t in range(0, numThreads):
+    os.wait()
+
 print("--Generating Movie out.mp4--")
 # Making the movie 
-cmd = "ffmpeg -hide_banner -loglevel error -framerate 20 -pattern_type glob -i \'images/*.jpeg\' -c:v libx264 -pix_fmt yuv420p out.mp4"
+cmd = "ffmpeg -hide_banner -loglevel error -framerate 30 -pattern_type glob -i \'images/*.jpeg\' -c:v libx264 -pix_fmt yuv420p out.mp4"
 os.system(cmd)
