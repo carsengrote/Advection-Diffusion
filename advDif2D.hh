700b83b5747e407be897ddf4edce904737bb32cd
MarkHan147 <66350720+MarkHan147@users.noreply.github.com>
2023-12-16 22:42:07 -0600

    Update advDif2D.hh

--- advDif2D.hh
+++ advDif2D.hh
@@ -5,7 +5,7 @@
 // i,k are x,z direction index
 // L,H are x,z direction array length
 
-void start(double LL, double HH, double dx, double T); 
+void start(double LL, double HH, double dx, double T);
 std::array<double, 2> convert(int i, int k, double dx);
 
 double initializeC(int i, int k); // return VALUE
@@ -16,12 +16,12 @@ double initializeUz(int i, int k, double LL, double HH);
 double CFL(double dx, double **ux, double **uz);
 
 bool isBoundary(int i, int k); // given index and bound
-void enforceBoundary (double **c);
+void enforceBoundary(double **c);
 
-double getC(int i, int k); 
-std::array<double, 2> getU(int i, int k); // return ux, uy, uz at one point
+double getC(double **c, int i, int k);
+std::array<double, 2> getU(double **ux, double **uz, int i, int k); // return ux, uy, uz at one point
 
-struct ptrStruct updateC(double ** c, double **cPrime, double **cPrimeLast, double **cNext, double **ux, double **uz, double dx, double dt, int E); 
+struct ptrStruct updateC(double **c, double **cPrime, double **cPrimeLast, double **cNext, double **ux, double **uz, double dx, double dt, int E);
 
 double **allocate2DArray();
 void deallocate2DArray(double **arr);
@@ -29,5 +29,10 @@ void process2DArray(double **arr);
 void computeC(double **c, double dx);
 void computeUx(double **ux, double dx, double LL, double HH);
 void computeUz(double **uz, double dx, double LL, double HH);
+
+double totalC(double **c);
+double avgC(double **c);
+void printVertical(double **c, double dx);
+void processU(double **ux, double **uz, double dx);
 
 #endif
